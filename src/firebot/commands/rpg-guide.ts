6f00183a72503d75a48a50ede3525b12b4d26fa5
Brett Slaski <brettski@yahoo.com>
2026-08-25 22:01:15 -0500

    remove firebottle references to templates
    Firebottle remains as author

--- src/firebot/commands/rpg-guide.ts
+++ src/firebot/commands/rpg-guide.ts
@@ -4,6 +4,6 @@ import { sendChatMessage } from '../firebot';
 export function rpgGuideCommand(userCommand: UserCommand) {
     const username = userCommand.commandSender;
 
-    const message = `@${username} You can view the in-depth player handbook here: https://github.com/Firebottle/firebot-rpg/wiki/Player-Handbook`;
+    const message = `@${username} You can view the in-depth player handbook here: https://github.com/brettski/firebot-rpg/wiki/Player-Handbook`;
     sendChatMessage(message);
 }
